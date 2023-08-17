f8d08c405d70400461ee2c917b40b6f035173352
Tesstarosa <raeinforce@gmail.com>
2023-08-17 19:28:38 +0300

    update interfaces

--- Interfaces/IPool.cs
+++ Interfaces/IPool.cs
@@ -1,22 +1,14 @@
 namespace PEPEngineers.PEPools.Interfaces
 {
-	public interface IPool<T> : IReadOnlyPool<T> where T : class
-	{
-		void Clear();
-	}
-
-	public interface IReadOnlyPool<T> where T : class
+	public interface IPool<T> where T : class
 	{
 		int FreeCount { get; }
-
 		int RentedCount { get; }
-
 		int MaxCount { get; }
-
 		int BucketSize { get; }
 
 		T Get();
-
 		void Release(T element);
+		void Clear();
 	}
 }
