a1f56a9fef951b0ae984a021b886ea33d7b8a6f6
Mikhanil <raeinforce@gmail.com>
2024-03-02 23:45:31 +0300

    fix remove issue
    add odin inspector attributes

--- Runtime/Pool.cs
+++ Runtime/Pool.cs
@@ -1,87 +1,125 @@
 using System;
 using System.Collections.Generic;
-using PEPEngineers.PEPools.Attributes;
-using PEPEngineers.PEPools.Extensions;
 using PEPEngineers.PEPools.Interfaces;
+using PEPEngineers.PEPools.Extensions;
 using PEPEngineers.PEPools.Settings;
-using UnityEngine;
+#if UNITY_EDITOR
 using UnityEngine.Assertions;
+#endif
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
 
 namespace PEPEngineers.PEPools.Runtime
 {
 	[Serializable]
 	public class Pool<T> : IPool<T> where T : class
 	{
-		[SerializeField] [ReadOnly] private int countAll;
-		[SerializeField] [ReadOnly] private int bucketSize;
-		[SerializeReference] [ReadOnly] private List<T> freeList;
-		[SerializeField] [ReadOnly] private int maxSize;
 		private readonly IPoolCallback<T> callbacks;
 		private readonly IPoolFactory<T> factory;
+		private int countAll;
+
 
 		public Pool(IPoolFactory<T> factory, IPoolCallback<T> callbacks = null,
 			int defaultCapacity = PoolConstants.DefaultBucketSize,
 			int bucketSize = PoolConstants.DefaultBucketSize,
 			int maxSize = PoolConstants.DefaultMaxSize)
 		{
+#if UNITY_EDITOR
 			Assert.IsFalse(maxSize <= 0);
-			freeList = new List<T>(defaultCapacity);
+			Assert.IsNotNull(factory);
+#endif
+			Items = new List<T>(defaultCapacity);
 			this.factory = factory;
 			this.callbacks = callbacks;
-			this.maxSize = maxSize;
-			this.bucketSize = bucketSize;
+			MaxCount = maxSize;
+			BucketSize = bucketSize;
 		}
 
+#if ODIN_INSPECTOR
+		[ReadOnly]
+		[ShowInInspector] 
+#endif
+		private List<T> Items { get; }
+
+#if ODIN_INSPECTOR
+		[ReadOnly]
+		[ShowInInspector]
+#endif
 		public int RentedCount => countAll - FreeCount;
-		public int MaxCount => maxSize;
-		public int BucketSize => bucketSize;
-		public int FreeCount => freeList.Count;
+
+#if ODIN_INSPECTOR
+		[ReadOnly]
+		[ShowInInspector]
+#endif
+		public int MaxCount { get; private set; }
+
+#if ODIN_INSPECTOR
+		[ReadOnly]
+		[ShowInInspector]
+#endif
+		public int BucketSize { get; private set; }
+
+#if ODIN_INSPECTOR
+		[ReadOnly]
+		[ShowInInspector]
+#endif
+		public int FreeCount => Items.Count;
 
 		public T Get()
 		{
 			if (FreeCount == 0) CreateBucket();
 
-			var item = freeList[0];
+			var item = Items[0];
 			callbacks?.OnItemRented(item);
-			freeList.FastRemove(0);
-			
+			Items.FastRemove(0);
+
 			return item;
 		}
 
 		public void Release(T element)
 		{
+#if UNITY_EDITOR
+			if (Items.Count > 0)
+				if (Items.Contains(element))
+					throw new InvalidOperationException("Trying to release an object that has already been released to the pool.");
+#endif
+
 			callbacks?.OnItemReleased(element);
-			if (FreeCount >= maxSize)
+			if (FreeCount >= MaxCount)
 			{
 				callbacks?.OnItemDestroyed(element);
 				factory.Destroy(element);
 			}
 			else
 			{
-				freeList.Add(element);
+				Items.Add(element);
 			}
 		}
 
 		public void Clear()
 		{
-			var count = freeList.Count;
-			while (count-- > 0) 
-				callbacks?.OnItemDestroyed(freeList[count]);
+			var count = Items.Count;
+			while (count-- > 0)
+			{
+				callbacks?.OnItemDestroyed(Items[count]);
+				factory.Destroy(Items[count]);
+			}
 
-			freeList.Clear();
+			Items.Clear();
 			countAll = 0;
 		}
 
 		private void CreateBucket()
 		{
-			for (var i = 0; i < bucketSize; i++)
+			for (var i = 0; i < BucketSize; i++)
 			{
 				var item = factory.Create();
-				freeList.Add(item);
 				callbacks?.OnItemCreated(item);
+				Items.Add(item);
 			}
 
-			countAll += bucketSize;
+			countAll += BucketSize;
 		}
 	}
 }
