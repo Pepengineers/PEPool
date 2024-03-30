5debff97178e4dd62a06bf9d95a447a156ddeb1a
Mikhanil <raeinforce@gmail.com>
2024-03-30 23:48:41 +0300

    decompose pool interface

--- Interfaces/IPool.cs
+++ Interfaces/IPool.cs
@@ -1,14 +1,17 @@
 namespace PEPEngineers.PEPools.Interfaces
 {
-	public interface IPool<T> where T : class
+	public interface IPool
 	{
 		int FreeCount { get; }
 		int RentedCount { get; }
 		int MaxCount { get; }
-		int BucketSize { get; }
-
+		int BucketSize { get; }		
+		void Clear();		
+	}
+	
+	public interface IPool<T> : IPool where T : class
+	{
 		T Get();
 		void Release(T element);
-		void Clear();
 	}
 }
